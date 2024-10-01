ab9e5f21eeedbcb3b2ff3465df86d1d28aacde68
Costajosue <conta2josue@gmail.com>
2024-10-01 09:12:44 -0300

    manipulando textos e aprendendo sobre funções

---
+++ JavaScript/Alura - Formação JavaScript/Lógica de Programação pt2 - JavaScript/app.js
@@ -0,0 +1,7 @@
+// Manipulando texto "<h1></h1>":
+let titulo = document.querySelector('h1'); // Criando uma variavel "titulo" e utilizando o document.querySelector('h1') para selecionar e alterar texto no HTML.
+titulo.innerHTML = 'Jogo do Número secreto';  // inserir um texto 
+
+// Manipulando texto "<p></p>":
+let paragrafo = document.querySelector('p'); // Criando uma variavel "paragrafo" e utilizando o document.querySelector('p') para selecionar e alterar texto no HTML.
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10:'; // inserir um texto 
