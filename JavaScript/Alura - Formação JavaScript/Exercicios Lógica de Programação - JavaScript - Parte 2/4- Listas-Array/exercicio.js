0b9dcc2e68378bff37fd5990fe5907b15d67c285
Costajosue <conta2josue@gmail.com>
2024-10-16 19:50:11 -0300

    Resolvendo exercicios de array

--- JavaScript/Alura - Formação JavaScript/Exercicios Lógica de Programação - JavaScript - Parte 2/4- Listas-Array/exercicio.js
+++ JavaScript/Alura - Formação JavaScript/Exercicios Lógica de Programação - JavaScript - Parte 2/4- Listas-Array/exercicio.js
@@ -1,24 +1,28 @@
 // 1- Crie uma lista vazia, com o nome listaGenerica.
-
+let listaGenerica = [];
 
 
 // 2- Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
-
+// let linguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin', 'Python'];
 
 
 // 3- Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
-
+let linguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin', 'Python'];
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
 
 
 // 4- Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
-
+let Tresnomes = ['Josue', 'Maria', 'Leandro'];
+console.log(Tresnomes[0]);
 
 
 // 5- Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
-
+let nomes = ['Josue', 'Maria', 'Leandro'];
+console.log(Tresnomes[1]);
 
 
 // 6- Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
-
+let tresNomes = ['Josue', 'Maria', 'Leandro'];
+console.log(Tresnomes[2]);
 
 
