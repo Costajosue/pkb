35a0f64233a953c897ba4c0343108ba12769aa91
Costajosue <conta2josue@gmail.com>
2025-04-09 17:33:18 -0300

    realizando exercicio

---
+++ Bc Systems Analysis & Development/Javascript/Desenvolvimento em Javascript/APIs - Bibliotecas Para Desenvolvimento Em Javascript/Ex 1/Exercicio1.js
@@ -0,0 +1,18 @@
+/*CÓDIGO PARA VERIFICAÇÃO DE MAIORIDADE*/
+
+// Solicita ao usuário que informe a idade, converte o valor para inteiro e armazena na variável "idade"
+let idade = parseInt(prompt('Informe sua idade: '));
+let body = document.body;
+let msg = document.getElementById('mensagem');
+
+if (idade <= 18) {
+    body.style.background = 'Darkred';
+    msg.style.fontSize = 'xx-large';
+    msg.style.color = 'Cornsilk';
+    msg.innerHTML = 'Você é menor de idade';
+} else {
+    body.style.background = 'Aquamarine';
+    msg.style.fontSize = 'xx-large';
+    msg.style.color = 'CadeBlue';
+    msg.innerHTML = 'Você é maior de idade';
+}
