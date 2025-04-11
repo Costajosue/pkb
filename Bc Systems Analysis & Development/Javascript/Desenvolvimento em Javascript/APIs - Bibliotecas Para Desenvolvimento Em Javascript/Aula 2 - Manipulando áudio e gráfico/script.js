2299a70a7ab080888af6f09d19cd14ad0f5f95d8
Costajosue <conta2josue@gmail.com>
2025-04-11 11:10:34 -0300

    Finalizando aula 02

---
+++ Bc Systems Analysis & Development/Javascript/Desenvolvimento em Javascript/APIs - Bibliotecas Para Desenvolvimento Em Javascript/Aula 2 - Manipulando áudio e gráfico/script.js
@@ -0,0 +1,25 @@
+// iniciando o canvas 
+var canvas = document.getElementById('progress') //  pega o elemento <canvas> do HTML que tem o id="progress"
+var ctx = canvas.getContext('2d'); //  ctx.fillRect() para desenhar retângulo 
+var atualiza = setInterval(animacao, 100); // chama a função animacao() a cada 100 milissegundos (0,1 segundo), repetidamente.
+
+// configurações
+var x = 0;
+var y = 0;
+var altura = 10;
+var largura = 0;
+var fator = 60;
+var resolucao = 2000;
+
+// cor da barra requisitada pela equipe
+ctx.fillStyle = "#4169E1"; 
+
+// função animada da barra de progresso
+function animacao(){
+    ctx.fillRect(x, y, largura = largura+fator, altura);
+
+    // codigo avançado: interrompe a função setInterval() para evitar carregamento excessivo.
+    if(largura > resolucao){
+        clearInterval(atualiza);
+    }
+}
