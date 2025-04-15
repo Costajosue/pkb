e4c3d87213deb49f04e221e10a4487eaff1773d3
Costajosue <conta2josue@gmail.com>
2025-04-15 16:27:43 -0300

    iniciando projeto sorteador de numeros - back-end

---
+++ JavaScript/Alura - Formação JavaScript/Lógica de Programação - JavaScript - Parte 3/Projeto Sorteador de Números/app.js
@@ -0,0 +1,10 @@
+// Criando uma função sortear que será para o botão / armazenando as info. quantidade, de e ate em uma variavel.
+function sortear(){
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+
+    alert(`quantidade: ${quantidade}`);
+    alert(`de: ${de}`);
+    alert(`ate: ${ate}`);
+}
