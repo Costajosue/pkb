dc55d542f05f16a383a0f220fad40ffd122cd62f
Costajosue <conta2josue@gmail.com>
2025-06-09 21:16:14 -0300

    atualizando

--- JavaScript/Alura - Formação JavaScript/Lógica de Programação - JavaScript - Parte 3/Projeto Sorteador de Números/app.js
+++ JavaScript/Alura - Formação JavaScript/Lógica de Programação - JavaScript - Parte 3/Projeto Sorteador de Números/app.js
@@ -4,11 +4,48 @@ function sortear(){
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
 
+    
+    // definindo as variaveis 
+    let sorteados = [];
+    let numero;
 
+    
+    for (let i = 0; i < quantidade; i++) {
+        numero = obterNumeroAleatorio(de, ate);
+
+        while (sorteados.includes(numero)) {
+            numero = obterNumeroAleatorio(de, ate);
+    }
+
+        sorteados.push(numero);
+    }
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`
+    alterarStatusdoBotao();
 
 }  
 // Criando uma função para obter um numero aleatorio 
 function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min; // função para gerar numero aleatorio;
 
+}
+
+function alterarStatusdoBotao(){
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else{
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar() {
+        document.getElementById('quantidade').value = '';
+        document.getElementById('de').value = '';
+        document.getElementById('ate').value = '';
+        document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>';
+        alterarStatusBotao();
 }
